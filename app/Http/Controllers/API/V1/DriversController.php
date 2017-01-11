60350057ddcbd2829a66585b1c5300cb2b1631c5
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2017-01-11 22:57:37 +0000

    Make an api controller for get x quantity of origin/clients pair from the database.

---
+++ app/Http/Controllers/API/V1/DriversController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Pulpomatic\Http\Controllers\API\V1;
+
+use Illuminate\Http\Request;
+use Pulpomatic\Http\Controllers\Controller;
+
+use Pulpomatic\Http\Requests\RoutesQuantityRequest;
+
+use Pulpomatic\Route;
+
+class DriversController extends Controller
+{
+  public function quantity( RoutesQuantityRequest $request )
+  {
+    dd( $request->quantity );
+
+    $addresses = Route::take( $request->quantity )
+                      ->get();
+
+    return $addresses;
+  }
+}
